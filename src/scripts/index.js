2488d394f3b51e0fb24ea219bb27b81b6f280b9c
ninest <parth.kabra@gmail.com>
2020-05-19 21:45:30 +0800

    added styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,32 @@
-import {
-  getHighscore,
-  setHighscore
-} from './highscore.js';
+import { getHighscore, setHighscore } from './highscore.js';
+import { hide, show } from './utils.js';
 import words from '~/assets/words.yml';
 
+const $startText = document.getElementById('start_text');
 const $currentWord = document.getElementById('current_word');
 const $textField = document.getElementById('text_field');
 const $time = document.getElementById('time');
+const $finalScoreWrapper = document.getElementById('final_score_wrapper');
+const $finalScore = document.getElementById('final_score');
 const $score = document.getElementById('score');
 const $highscore = document.getElementById('highscore');
 
 var currentWord = '';
 var inGame = false;
 var time = 0; // milliseconds
+const gameLenght = 5; // milliseconds
 var score = 0;
 var interval;
 
 document.body.onload = () => {
-  $currentWord.innerText = 'Type "start" to play';
-  $textField.focus();
+  hide($currentWord);
+  $time.innerText = `${gameLenght}s`;
 
+  $textField.focus();
   $textField.oninput = textFieldUpdate;
+
+  // hide final score, should only show at end of game
+  hide($finalScoreWrapper);
 
   $highscore.innerText = getHighscore();
 };
@@ -38,12 +44,18 @@ const startGame = () => {
   inGame = true;
   score = 0;
 
-  // 5 seconds to start
-  round(5);
+  // start round
+  round(gameLenght);
+
+  show($currentWord);
+  hide($startText);
+
+  // hide final score, should only show at end of game
+  hide($finalScoreWrapper);
 
   interval = setInterval(() => {
     time--;
-    $time.innerText = time;
+    $time.innerText = `${time}s`;
     if (time <= 0) {
       endGame();
     }
@@ -74,6 +86,16 @@ const endGame = () => {
 
   inGame = false;
 
+  // show start text and change text
+  show($startText);
+  $startText.innerHTML = `
+    <span class="game-over">Game over. </span> Type "<b>start</b> to play</span>
+  `;
+
+  // show final score
+  $finalScore.innerText = score;
+  show($finalScoreWrapper);
+
   // check if score more than highscore
   const highscore = getHighscore();
   if (score > highscore) {
@@ -83,6 +105,6 @@ const endGame = () => {
     setHighscore(score);
   }
 
-  $currentWord.innerText = 'Type "start" to play again';
+  hide($currentWord);
   $textField.value = '';
 };
---
+++ src/scripts/utils.js
@@ -0,0 +1,9 @@
+const hide = (elem) => {
+  elem.style.display = 'none';
+};
+
+const show = (elem) => {
+  elem.style.display = 'block';
+};
+
+export { hide, show };
