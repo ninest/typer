8e9bb9f05268803aa578b6a992f848a2c640e711
ninest <parth.kabra@gmail.com>
2020-05-19 14:26:46 +0800

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  rules: {
+    semi: [2, 'always']
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import {
   getHighscore,
-  setHighscore,
+  setHighscore
 } from './highscore.js';
 
 const $currentWord = document.getElementById('current_word');
 const $textField = document.getElementById('text_field');
 const $time = document.getElementById('time');
-const $score = document.getElementById('score')
-const $highscore = document.getElementById('highscore')
+const $score = document.getElementById('score');
+const $highscore = document.getElementById('highscore');
 
-const words = ["one", "two", "three", "four", "computer", "stewardess", "four-twenty", "javascript", "large-headed donkey"];
+const words = ['one', 'two', 'three', 'four', 'computer', 'stewardess', 'four-twenty', 'javascript', 'large-headed donkey'];
 var currentWord = '';
 
 var inGame = false;
@@ -18,13 +18,13 @@ var score = 0;
 var interval;
 
 document.body.onload = () => {
-  $currentWord.innerText = 'Type "start" to play'
+  $currentWord.innerText = 'Type "start" to play';
   $textField.focus();
 
   $textField.oninput = textFieldUpdate;
 
   $highscore.innerText = getHighscore();
-}
+};
 
 const textFieldUpdate = () => {
   const text = $textField.value.toLowerCase();
@@ -33,15 +33,14 @@ const textFieldUpdate = () => {
 
   // if word guessed correct, get an extra second
   if (inGame && text === currentWord) round(1);
-}
+};
 
 const startGame = () => {
   inGame = true;
   score = 0;
-  
+
   // 5 seconds to start
   round(5);
-
 
   interval = setInterval(() => {
     time--;
@@ -50,43 +49,41 @@ const startGame = () => {
       endGame();
     }
   }, 1000);
-}
-
+};
 
 const round = (val) => {
   // clear the textfield
-  $textField.value = "";
+  $textField.value = '';
 
   increaseTime(val);
-  score ++;
+  score++;
   $score.innerText = score;
-  
+
   // get random word
-  currentWord = words[Math.floor(Math.random() * words.length)]
+  currentWord = words[Math.floor(Math.random() * words.length)];
   $currentWord.innerText = currentWord;
-}
+};
 
 // when word typed
 const increaseTime = (val) => {
   time = time + val;
-}
+};
 
 const endGame = () => {
   // reset timer
   clearInterval(interval);
 
   inGame = false;
-  
+
   // check if score more than highscore
   const highscore = getHighscore();
   if (score > highscore) {
-    console.log('high score has been beaten!')
+    console.log('high score has been beaten!');
     // set new highscore
     $highscore.innerText = score;
     setHighscore(score);
   }
 
-
-  $currentWord.innerText = 'Type "start" to play again'
+  $currentWord.innerText = 'Type "start" to play again';
   $textField.value = '';
-}
+};
