7f4aa6bde2da9eafcba3c1847ed828a076787e08
ninest <parth.kabra@gmail.com>
2020-05-30 18:29:40 +0800

    removed some console poop

--- src/scripts/anticheat.js
+++ src/scripts/anticheat.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
   // nothing here
 } else {
   window.addEventListener('devtoolschange', event => {
-    console.log('Is DevTools open:', event.detail.isOpen);
+    // console.log('Is DevTools open:', event.detail.isOpen);
     location.reload();
     window.location.replace('https://github.com/ninest/typer');
     setHighscore(0);
