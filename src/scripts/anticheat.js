43f55d88206144952668c7a1d09a1f566f7b7da5
ninest <parth.kabra@gmail.com>
2020-05-31 14:32:45 +0800

    formatted with eslint

--- src/scripts/anticheat.js
+++ src/scripts/anticheat.js
@@ -1,18 +1,18 @@
 import 'devtools-detect';
 import { setHighscore } from './highscore.js';
 
-//initial check for webdriver, possible automation
-if(navigator.webdriver == true){
+// initial check for webdriver, possible automation
+if (navigator.webdriver === true) {
   location.reload();
   window.location.replace('https://github.com/ninest/typer');
   setHighscore(0);
 }
 
+// open github repo if dev tools opened
 if (process.env.NODE_ENV === 'development') {
   // nothing here
 } else {
   window.addEventListener('devtoolschange', event => {
-    // console.log('Is DevTools open:', event.detail.isOpen);
     location.reload();
     window.location.replace('https://github.com/ninest/typer');
     setHighscore(0);
